42752a10599d9b20329756b1f0b90fff9371fe63
Kevin Liou <kevin.liou@moovweb.com>
2013-10-21 20:22:24 -0700

    allow no line numbers

--- shInit.js
+++ shInit.js
@@ -2,6 +2,6 @@ $(function() {
   var d = SyntaxHighlighter.defaults;
   d["quick-code"] = d["toolbar"] = false;
   $("pre").each(function(_, c) {
-    SyntaxHighlighter.highlight({ brush: $(c).attr("data-lang") }, c);
+    SyntaxHighlighter.highlight({ brush: $(c).attr("data-lang"), gutter: !$(c).is("[data-no-gutter]") }, c);
   });
 });
